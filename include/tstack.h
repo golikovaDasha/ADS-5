9748bdbe6dd5219aca9d55cdb530860802c7efac
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-03-29 15:03:46 +0300

    Голикова 21-ИСТ-3

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-
+private:
+  T arr[100];
+   int T;
+public:
+  TStack():top(-1){}
+  T get()const {//взять
+    return arr[top];
+  }
+  bool isEmpty() const {
+    return top==-1;//пустота
+  }
+  bool isFull() const {
+    return top==size-1;//полнота
+  }
+  void pop() {
+  if(!isEmpty())
+    top--;
+  }
+  T get() {
+    if(isEmpty())
+      throw "empty!";
+    else
+      return arr[top];
+  }
+  void push() {
+  if (isFull())
+    throw "full";
+  else
+    arr[++top]=value;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
